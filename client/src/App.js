e8b1b791a667bab50670cf62fe917e3d092fb1dd
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-26 15:02:13 -0400

    updated Todo function with a btn that when clicked will put a line through the selected todo

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+const Todo = ({ todo, index, completeTodo }) => {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
+    </div>
+  );
+};
 
 function TodoForm({ addTodo }) {
   const [value, setValue] = useState("");
@@ -37,11 +50,22 @@ function App() {
     setTodos(newTodos);
   };
 
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+          />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
