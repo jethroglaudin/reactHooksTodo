3ded4359cde9023081aa27ad0903072f45eae8df
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-26 13:54:00 -0400

    imported useState from react. created todos as the name of the state and setTodos to what goign to use set the state

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState }from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
+  ]);
+
+  const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
   return (
     <div className="App">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
       
     </div>
   );
